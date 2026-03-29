95950f1367e7b198f4b5b72101267644e2822d5c
aitoAarni <aarni.rechardt@gmail.com>
2026-03-29 19:13:03 +0300

    add function to create inteval sums

--- week2/include/bit_array.hpp
+++ week2/include/bit_array.hpp
@@ -2,39 +2,54 @@
 #include <iostream>
 #include <cstdint>
 
+using ui = uint64_t;
 
 class BitArray {
-    uint64_t* data;
+    ui* data;
+    ui size;
+    ui* interval_sums;
+
+    void build_interval_sums() {
+        interval_sums = new ui[size];
+        if (size > 0) interval_sums[0] = __builtin_popcountl(data[0]);
+        for (int i {0}; i < size; i++) {
+            interval_sums[i] = interval_sums[i-1] + __builtin_popcountl(data[i]);
+        }
+    }
 
     public:
-    BitArray(uint64_t m) : data(new uint64_t[(63+m)/64]()) {}
+    BitArray(ui m) : data(new ui[(63+m)/64]()), size((63+m)/64), interval_sums(nullptr) {}
     ~BitArray() {
         delete[] data;
     }
 
-    void set(uint64_t index) {
+    void set(ui index) {
         auto array_index {index / 64};
         auto bit_index {index % 64};
         auto& number {data[array_index]};
-        uint64_t modifier {uint64_t(1) << (bit_index )};
-        // if ((modifier & bit_index) == (uint64_t)(0)) {
-        number = number | modifier;
-        // }
+        ui modifier {ui(1) << (bit_index )};
+        number |=  modifier;
     }
 
 
 
-    bool get(uint64_t index) {
+    bool get(ui index) {
 
         auto array_index {index / 64};
         auto bit_index {index % 64};
 
         auto& number {data[array_index]};
-        uint64_t modifier {uint64_t(1) << (bit_index )};
+        ui modifier {ui(1) << (bit_index )};
         return (modifier & number) != 0;
     }
 
-    uint64_t sum(uint64_t index) {
+    
+
+
+    ui sum(ui index) {
+        if (interval_sums == nullptr) {
+            build_interval_sums();
+        }
         return index;
     }
 };
